870300a682bd0b53e2dd0f34ef3fbe04c24bfd60
leob03 <87416230+leob03@users.noreply.github.com>
2023-06-30 00:18:45 -0400

    Create train_lstm.py
    no main(), not saving the trained model

---
+++ train_lstm.py
@@ -0,0 +1,32 @@
+
+
+reset_seed(0)
+
+# data input
+small_num_train = num_train
+sample_idx = torch.randint(num_train, size=(small_num_train,))
+small_image_data = data_dict["train_images"][sample_idx]
+small_caption_data = data_dict["train_captions"][sample_idx]
+
+# create the image captioning model
+lstm_model = CaptioningRNN(
+    cell_type="lstm",
+    word_to_idx=data_dict["vocab"]["token_to_idx"],
+    input_dim=400,  # hard-coded, do not modify
+    hidden_dim=512,
+    wordvec_dim=256,
+    ignore_index=NULL_index,
+)
+# lstm_model = lstm_model.to(DEVICE)
+
+for learning_rate in [1e-3]:
+    print("learning rate is: ", learning_rate)
+    lstm_model_submit, lstm_loss_submit = train_captioner(
+        lstm_model,
+        small_image_data,
+        small_caption_data,
+        num_epochs=60,
+        batch_size=BATCH_SIZE,
+        learning_rate=learning_rate,
+        device=DEVICE,
+    )

73a60e03ae01dbf549f2760b305f618a6bc7af36
leob03 <87416230+leob03@users.noreply.github.com>
2023-06-30 00:21:47 -0400

    Create train_lstm_attention.py

---
+++ train_lstm_attention.py
@@ -0,0 +1,35 @@
+from rnn_lstm_captioning import AttentionLSTM, ImageEncoder, WordEmbedding, temporal_softmax_loss, CaptioningRNN
+
+model = ImageEncoder(pretrained=True, verbose=True).to(device=DEVICE)
+
+from a5_helper import train_captioner
+
+reset_seed(0)
+
+# data input
+small_num_train = num_train
+sample_idx = torch.randint(num_train, size=(small_num_train,))
+small_image_data = data_dict["train_images"][sample_idx]
+small_caption_data = data_dict["train_captions"][sample_idx]
+
+# create the image captioning model
+attn_model = CaptioningRNN(
+    cell_type="attn",
+    word_to_idx=data_dict["vocab"]["token_to_idx"],
+    input_dim=400,  # hard-coded, do not modify
+    hidden_dim=512,
+    wordvec_dim=256,
+    ignore_index=NULL_index,
+)
+attn_model = attn_model.to(DEVICE)
+
+for learning_rate in [1e-3]:
+    print("learning rate is: ", learning_rate)
+    attn_model_submit, attn_loss_submit = train_captioner(
+        attn_model,
+        small_image_data,
+        small_caption_data,
+        num_epochs=60,
+        batch_size=BATCH_SIZE,
+        learning_rate=learning_rate,
+        device=DEVICE,
